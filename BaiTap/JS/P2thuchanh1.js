dc24fc18d65102d5214ec8f4052a47f72ac11061
tranthenhu <tranthenhu150@yahoo.com>
2019-01-08 13:45:12 +0700

    update

---
+++ BaiTap/JS/P2thuchanh1.js
@@ -0,0 +1,11 @@
+var i = 10;
+var f = 20.5;
+var b = true;
+var s = "ha noi";
+document.write("i = " + i);
+document.write("<br/>");
+document.write("f = " + f);
+document.write("<br/>");
+document.write("b = " + b);
+document.write("<br/>");
+document.write("s = " + s);
---
+++ BaiTap/JS/P2thuchanh3.js
@@ -0,0 +1,7 @@
+a = prompt("a ",'');
+b = prompt("b",'');
+if (a%b ==0) {
+    alert("a la boi so cua b");
+}else  {
+    alert("a k la boi so cua b ");
+}
